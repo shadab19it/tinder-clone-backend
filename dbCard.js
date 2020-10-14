226216bc69fba71ab001c24e1faf757e3ef3d08d
Shadab <shadab19it@gmail.com>
2020-10-14 15:48:55 +0530

    first commit

---
+++ dbCard.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const cardSchema = mongoose.Schema({
+  name: String,
+  imgUrl: String,
+});
+const Cards = mongoose.model("cards", cardSchema);
+export default Cards;
---
+++ server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import Cards from "./dbCard.js";
+import cors from "cors";
+dotenv.config();
+
+// App Config
+const app = express();
+// DB Config
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("db Connected");
+  })
+  .catch((err) => console.log(err));
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// API Endpoint
+app.get("/", (req, res) => {
+  res.status(200).send("hellow");
+});
+
+app.post("/tinder/cards", (req, res) => {
+  const cardInfo = req.body;
+
+  Cards.create(cardInfo, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(201).send(data);
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(201).send(data);
+  });
+});
+
+// App listner
+const PORT = 9000;
+app.listen(PORT, () => {
+  console.log(`Server Start with port ${PORT} ==> http://localhost:${PORT} `);
+});
